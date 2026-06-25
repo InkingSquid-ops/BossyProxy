13b5914bf91e0ea966fa0d268f3446c15fa6c558
InkingSquid-ops <InkingSquid1@proton.me>
2026-06-25 01:35:24 +0000

    Rename and fix HTML node rewrite function

--- Backend/Parser/Html/htmlNode.go
+++ Backend/Parser/Html/htmlNode.go
@@ -6,17 +6,17 @@ import (
 	"golang.org/x/net/html"
 )
 
-func rewriteHTMLnode(doc *html.Node, base *url.URL) {
+func RewriteHTMLnode(doc *html.Node, base *url.URL) {
 	if doc.Type == html.ElementNode {
 		for i := range doc.Attr {
 			switch doc.Attr[i].Key {
 			case "href", "src":
-				doc.Attr[i].Key = BrowseURL(doc.Attr[i].Key, base)
+				doc.Attr[i].Val = BrowseURL(doc.Attr[i].Val, base)
 			}
 		}
 	}
 
 	for c := doc.FirstChild; c != nil; c = c.NextSibling {
-		rewriteHTMLnode(doc, base)
+		RewriteHTMLnode(c, base)
 	}
 }
