553c4f56421a8c9b2c0a3ecb12399eb00fec0271
InkingSquid-ops <InkingSquid1@proton.me>
2026-06-25 22:46:40 +0000

    Enhance URL validation with additional protocols

--- Backend/Security/URLparser.go
+++ Backend/Security/URLparser.go
@@ -1,13 +1,23 @@
 package Security
 
 import (
+	"fmt"
 	"strings"
 )
 
 func ValidateURL(raw string) string {
-	if !strings.Contains(raw, "https://") && !strings.Contains(raw, "http://") {
-		newRaw := "https://" + raw
-		return newRaw
+	switch {
+	case strings.Contains(raw, "file://"):
+		return fmt.Sprintf("Invalid url: %s", raw)
+
+	case strings.Contains(raw, "ftp://"):
+		return fmt.Sprintf("Invalid url: %s", raw)
+
+	default:
+		if !strings.Contains(raw, "https://") && !strings.Contains(raw, "http://") {
+			newRaw := "https://" + raw
+			return newRaw
+		}
 	}
 
 	return raw
