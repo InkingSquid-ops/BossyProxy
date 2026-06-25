901698a82bbce7ddbe38eddfd0d2073adf753045
InkingSquid-ops <InkingSquid1@proton.me>
2026-06-25 01:32:47 +0000

    Enhance Fetch function with URL check and rewrite
    Added URL validation and HTML rewriting in Fetch function.

--- Backend/Services/proxy.go
+++ Backend/Services/proxy.go
@@ -1,12 +1,19 @@
 package Services
 
 import (
-	"io"
 	"net/http"
+	"net/url"
+
+	HTML "github.com/InkingSquid-ops/BossyProxy/Backend/Parser/Html"
 )
 
-func Fetch(w http.ResponseWriter, url string) {
-	resp, err := http.Get(url)
+func Fetch(w http.ResponseWriter, TargetUrl string) {
+	if TargetUrl == "" {
+		http.Error(w, "missing url parameter", http.StatusBadRequest)
+		return
+	}
+
+	resp, err := http.Get(TargetUrl)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadGateway)
@@ -15,5 +22,20 @@ func Fetch(w http.ResponseWriter, url string) {
 
 	defer resp.Body.Close()
 
-	io.Copy(w, resp.Body)
+	baseURL, err := url.Parse(TargetUrl)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := HTML.RewriteHTML(resp.Body, baseURL)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(result)
 }
+
