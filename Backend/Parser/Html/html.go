98756fbf9f96fafcfa67fae305eb824a6ee851c7
InkingSquid-ops <InkingSquid1@proton.me>
2026-06-25 01:34:26 +0000

    Rename rewriteHTML function to RewriteHTML

--- Backend/Parser/Html/html.go
+++ Backend/Parser/Html/html.go
@@ -8,14 +8,14 @@ import (
 	"golang.org/x/net/html"
 )
 
-func rewriteHTML(r io.Reader, base *url.URL) ([]byte, error) {
+func RewriteHTML(r io.Reader, base *url.URL) ([]byte, error) {
 	doc, err := html.Parse(r)
 
 	if err != nil {
 		return nil, err
 	}
 
-	rewriteHTMLnode(doc, base)
+	RewriteHTMLnode(doc, base)
 
 	var buf bytes.Buffer
 
@@ -25,3 +25,4 @@ func rewriteHTML(r io.Reader, base *url.URL) ([]byte, error) {
 
 	return buf.Bytes(), nil
 }
+
